e2f7c89a5384c7e1369b48937e6b9d925c8180d3
alexlake <alex@thewinelake.com>
2017-06-01 18:46:58 +0100

    MOrning

--- forms/frmLogin.php
+++ forms/frmLogin.php
@@ -15,8 +15,8 @@ include "furniture/header.php"
 <form name="login" id="login" method="POST" action="login">
     <p>Email:<input type="text" name="email" value="<?= $email ?>"></p>
     <p>Password:<input type="password" name="pwd"></p>
-    <p><a class="button login">login</a></p>
-    <p><a class="button showregister">register...</a></p>
+    <p><button class="login">login</button></p>
+    <p>Or if you're new around here, <a class="button showregister">register...</a></p>
 
 </form>
 
